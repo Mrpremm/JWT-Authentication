6ae011f4feb031a0e94d301e514148b9fb2e679b
Prem Prasad <premrinkinoniya@gmail.com>
2026-02-02 21:29:32 +0530

    Updated Code

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-require("dotenv").congfig();
+require("dotenv").config();
+const app=require("./src/app");
+const connectDB=require("./src/config/db");
+
+connectDB();
+const PORT=process.env.PORT|| 4000;
+app.listen(PORT,()=>{
+  console.log(`Server running on port ${PORT}`);
+  
+})
